d9abb51d9bbf2507469383c527c5e64ac2308c24
amit.deshwal <amit.deshwal@gupshup.io>
2023-07-13 01:48:14 +0530

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,15 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 import BaseLayout from '../components/BaseLayout'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <BaseLayout>
-      <Component {...pageProps} />
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
     </BaseLayout>
   )
 }
